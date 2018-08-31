d58e397f5d6963a53e7521a52f366d2c4f2d7fb5
Chris <aqui.christine@gmail.com>
2018-08-30 23:16:49 -0400

    checkpoint for week 19

---
+++ week19/reactpractice/src-03/components/HelloDiv.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HelloDiv = () => {
+  return (
+    <div>
+      <h1>Hello! my Name is Chris.</h1>
+      <h2>Shit I like</h2>
+      <ul>
+        <li>Women</li>
+        <li>Women</li>
+        <li>More Women</li>
+        <li>Motorbikes</li>
+      </ul>
+    </div>
+  )
+};
+
+export default HelloDiv;
